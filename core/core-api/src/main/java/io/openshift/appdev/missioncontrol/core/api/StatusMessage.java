cc95d74f97e08320568a1a4fb64c6c6583b54bcc
Andrew Lee Rubinger <alr@alrubinger.com>
2017-04-15 06:55:48 -0400

    Fixes #234 * Update status messages on Launch

--- core/core-api/src/main/java/io/openshift/appdev/missioncontrol/core/api/StatusMessage.java
+++ core/core-api/src/main/java/io/openshift/appdev/missioncontrol/core/api/StatusMessage.java
@@ -1,15 +1,16 @@
 package io.openshift.appdev.missioncontrol.core.api;
 
 /**
- * That holds all status messages that we send to the clients via websockets to inform them about the status of their project
+ * That holds all status messages that we send to the clients via
+ * websockets to inform them about the status of their project
  */
 public enum StatusMessage {
 
-    GITHUB_CREATE("created github repository"),
-    GITHUB_PUSHED("pushed code to github repository"),
-    OPENSHIFT_CREATE("openshift project created"),
-    OPENSHIFT_PIPELINE("openshift added pipeline to project"),
-    GITHUB_WEBHOOK("added webhook to github repository");
+    GITHUB_CREATE("Creating your new GitHub repository"),
+    GITHUB_PUSHED("Pushing your customized Booster code into the repo"),
+    OPENSHIFT_CREATE("Creating your project on the OpenShift cloud"),
+    OPENSHIFT_PIPELINE("Setting up your Build Pipeline"),
+    GITHUB_WEBHOOK("Configuring to trigger builds on Git pushes");
 
     StatusMessage(String message) {
         this.message = message;
