07e79ef1a27372b902c7285f538d6b50605d73a2
George Gastaldi <gegastaldi@gmail.com>
2017-04-18 15:39:50 -0300

    Fixes #236: Introduced message buffer for events fired before WS session is opened

--- web/src/main/java/io/openshift/appdev/missioncontrol/web/api/websocket/MissionControlStatusEndpoint.java
+++ web/src/main/java/io/openshift/appdev/missioncontrol/web/api/websocket/MissionControlStatusEndpoint.java
@@ -1,9 +1,12 @@
 package io.openshift.appdev.missioncontrol.web.api.websocket;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.event.Observes;
@@ -12,6 +15,7 @@ import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
+import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
@@ -31,23 +35,34 @@ public class MissionControlStatusEndpoint {
 
     private static final Map<UUID, Session> peers = new ConcurrentHashMap<>();
 
+    private static final Map<UUID, List<String>> messageBuffer = new ConcurrentHashMap<>();
+
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @OnOpen
     public void onOpen(Session session, @PathParam("uuid") String uuid) {
-        peers.put(UUID.fromString(uuid), session);
+        UUID key = UUID.fromString(uuid);
+        peers.put(key, session);
         JsonArrayBuilder builder = Json.createArrayBuilder();
         for (StatusMessage statusMessage : StatusMessage.values()) {
             JsonObjectBuilder object = Json.createObjectBuilder();
             builder.add(object.add(statusMessage.name(), statusMessage.getMessage()).build());
         }
 
-        session.getAsyncRemote().sendText(builder.build().toString());
+        RemoteEndpoint.Async asyncRemote = session.getAsyncRemote();
+        asyncRemote.sendText(builder.build().toString());
+        // Send pending messages
+        List<String> messages = messageBuffer.remove(key);
+        if (messages != null) {
+            messages.forEach(asyncRemote::sendText);
+        }
     }
 
     @OnClose
     public void onClose(@PathParam("uuid") String uuid) throws IOException {
-        peers.remove(UUID.fromString(uuid));
+        UUID key = UUID.fromString(uuid);
+        peers.remove(key);
+        messageBuffer.remove(key);
     }
 
     /**
@@ -57,11 +72,19 @@ public class MissionControlStatusEndpoint {
      * @throws IOException when message could not be serialized to JSON
      */
     public void onEvent(@Observes StatusMessageEvent msg) throws IOException {
-        Session session = peers.get(msg.getId());
+        UUID msgId = msg.getId();
+        Session session = peers.get(msgId);
+        String message = objectMapper.writeValueAsString(msg);
         if (session != null) {
-            session.getAsyncRemote().sendText(objectMapper.writeValueAsString(msg));
+            session.getAsyncRemote().sendText(message);
         } else {
-            log.warning(String.format("no active websocket session found for projectile with ID '%s'", msg.getId()));
+            List<String> messages = messageBuffer.get(msgId);
+            if (messages == null) {
+                messages = new ArrayList<>();
+                messageBuffer.put(msgId, messages);
+            }
+            messages.add(message);
+            log.log(Level.WARNING, "No active WebSocket session was found for projectile {0}", msgId);
         }
     }
 }
