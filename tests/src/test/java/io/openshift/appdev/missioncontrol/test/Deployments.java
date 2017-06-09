aa7b8295205600ad3e0ed22d7a9754a0a126fda6
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-06-09 15:42:25 +0200

    fixed IT test (#255)
    fixed IT test

--- tests/src/test/java/io/openshift/appdev/missioncontrol/test/Deployments.java
+++ tests/src/test/java/io/openshift/appdev/missioncontrol/test/Deployments.java
@@ -2,6 +2,11 @@ package io.openshift.appdev.missioncontrol.test;
 
 import java.io.File;
 
+import io.openshift.appdev.missioncontrol.service.github.api.GitHubService;
+import io.openshift.appdev.missioncontrol.service.github.api.GitHubServiceFactory;
+import io.openshift.appdev.missioncontrol.service.github.impl.kohsuke.KohsukeGitHubServiceImpl;
+import io.openshift.appdev.missioncontrol.service.github.spi.GitHubServiceSpi;
+import io.openshift.appdev.missioncontrol.service.openshift.api.OpenShiftServiceFactory;
 import io.openshift.appdev.missioncontrol.service.openshift.spi.OpenShiftServiceSpi;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -37,21 +42,33 @@ class Deployments {
                 .addPackages(
                         true,
                         OpenShiftServiceSpi.class.getPackage(),
-                        OpenShiftProjectImpl.class.getPackage())
-                .addPackage(WebDriverProviderHack.class.getPackage());
-        final File[] deps = Resolvers.use(MavenResolverSystem.class).
+                        OpenShiftProjectImpl.class.getPackage(),
+                        OpenShiftServiceFactory.class.getPackage(),
+                        GitHubServiceSpi.class.getPackage(),
+                        KohsukeGitHubServiceImpl.class.getPackage(),
+                        GitHubServiceFactory.class.getPackage())
+                .addPackage(TestSupport.class.getPackage());
+        final File[] depsOpenshift = Resolvers.use(MavenResolverSystem.class).
                 loadPomFromFile("../services/openshift-service-impl/pom.xml").
                 importRuntimeAndTestDependencies().
                 resolve().
                 withTransitivity().
                 asFile();
+        final File[] depsGithub = Resolvers.use(MavenResolverSystem.class).
+                loadPomFromFile("../services/github-service-impl/pom.xml").
+                importRuntimeAndTestDependencies().
+                resolve().
+                withTransitivity().
+                asFile();
+
         final File[] ourTestDeps = Resolvers.use(MavenResolverSystem.class).
                 loadPomFromFile("pom.xml").
                 importTestDependencies().
                 resolve().
                 withTransitivity().
                 asFile();
-        archive.addAsLibraries(deps);
+        archive.addAsLibraries(depsOpenshift);
+        archive.addAsLibraries(depsGithub);
         archive.addAsLibraries(ourTestDeps);
         return archive;
     }
